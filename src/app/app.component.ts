7e0a8e196e1e10a049cd5af19af0039947c19937
ypolosov <ypolosov@gmail.com>
2016-12-18 17:17:59 +0300

    npm deps updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import {VgAPI, VgFullscreenAPI} from "videogular2/core";
 
-import { TabsPage } from '../pages/tabs/tabs';
+// import { TabsPage } from '../pages/tabs/tabs';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+  // rootPage = TabsPage;
+  
+  
+  sources: Array<Object>;
+  track: Object;
+  controls: boolean = false;
+  autoplay: boolean = false;
+  loop: boolean = false;
+  preload: string = 'auto';
+  api: VgAPI;
+  fsAPI: VgFullscreenAPI;
+  
+  clickedCue: any;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
@@ -17,6 +30,77 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+  
+  
+      
+      
+      
     });
+  
+    this.fsAPI = VgFullscreenAPI;
+  
+    this.sources = [
+      {
+        src: "assets/movie/pale-blue-dot.mp4",
+        type: "video/mp4"
+      }
+    ];
+    this.track =
+      <any>{
+        kind: "subtitles",
+        label: "English",
+        src: "assets/movie/pale-blue-dot.vtt",
+        srclang: "en",
+        default: true
+      };
+  }
+  
+
+  
+  onPlayerReady(api: VgAPI) {
+    this.api = api;
+    
+    this.api.getDefaultMedia().subscriptions.ended.subscribe(
+      () => {
+        // Set the video to the beginning
+        this.api.getDefaultMedia().currentTime = 0;
+      }
+    );
+  }
+  
+  
+  onEnterCuePoint($event) {
+    let startTime = $event.startTime;
+    if (this.clickedCue && (startTime == this.clickedCue.startTime)) {
+      this.api.play();
+    }
+  }
+  
+  onExitCuePoint($event) {
+    let endTime = $event.endTime;
+    if (this.clickedCue && (endTime == this.clickedCue.endTime)) {
+      this.clickedCue = null;
+      this.api.pause();
+    }
+    
+  }
+  
+  onCueClick(cue: any) {
+    
+    this.clickedCue = cue;
+    this.api.currentTime = this.clickedCue.startTime;
+    this.api.play();
+    
+    // let tu = this.api.getDefaultMedia().subscriptions.timeUpdate;
+    // this.api.currentTime = startTime;
+    // this.api.play();
+    // let tuSub = tu.subscribe(
+    //     () => {
+    //         if (this.api.currentTime >= endTime) {
+    //             tuSub.unsubscribe();
+    //             this.api.pause();
+    //         }
+    //     }
+    // );
   }
 }
